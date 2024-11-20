850d449330165fa06bfa1169eaf8f5f87e04b28a
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2024-11-20 16:12:54 +0100

    Added comments

--- src/utils/calcRating.js
+++ src/utils/calcRating.js
@@ -1,11 +1,13 @@
+// Created for future use.
+// Calculates the accurate rating in property object based on
+// the ratings in reviews, instead of the current manual INT value.
+
 const calcRating = (property) => {
   if (property.reviews.length > 0) {
     let queryArray = property.reviews;
-    //console.log(property.title);
     let result = queryArray.reduce((total, item) => {
       return total + item.rating;
     }, 0);
-    //console.log(result / queryArray.length);
     return result / queryArray.length;
   } else if ((property.reviews.length = 0)) {
     return 0;
